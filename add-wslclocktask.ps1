194c76870659817ebbb9d63ca4d3efb2ab4055a9
Stuart Leeks <stuart@leeks.net>
2020-08-09 21:26:19 +0100

    fixup add script

--- add-wslclocktask.ps1
+++ add-wslclocktask.ps1
@@ -1,3 +1,3 @@
-$taskCommand = $PSScriptRoot + "\wsl-clock.exe'"
+$taskCommand = $PSScriptRoot + "\wsl-clock.exe"
 
 schtasks /Create /TN wsl-clock /TR $taskCommand /SC ONEVENT /EC System /MO "*[System[Provider[@Name='Microsoft-Windows-Kernel-Power'] and (EventID=107 or EventID=507)]]" /F
